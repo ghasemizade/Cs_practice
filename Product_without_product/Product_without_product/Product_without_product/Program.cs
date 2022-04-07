e1d860144adc690a5b2db1d8168648b6e5e90320
Hosein <mrqasemizade@gmail.com>
2022-04-07 14:59:31 +0430

    Product_without_product

---
+++ Product_without_product/Product_without_product/Product_without_product/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter two number for calcute product :");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int pro1 = 0;
+            for (int counter = 1; counter <= num2; counter++)
+            {
+                pro1 += num1;
+            }
+            Console.WriteLine(pro1);
+        }
+    }
+}
