261e6c04970d9eb550d1777dc7454995ebf5aa20
Hosein <mrqasemizade@gmail.com>
2022-04-07 16:33:49 +0430

    Draw rectangle

---
+++ Draw_rectangle/draw_rectangle/draw_rectangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("height :");
+            int side = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("width :");
+            int side1 = Convert.ToInt32(Console.ReadLine());
+            int counter = 0;
+            for (int height = 0; height < side; height++)
+            {
+                for (int width = 0; width < side1; width++)
+                {
+                    if (height == counter)
+                        Console.Write("*");
+                    else
+                        Console.Write("*");
+                }
+                counter++;
+
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
