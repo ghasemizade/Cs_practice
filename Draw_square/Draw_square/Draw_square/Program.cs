535f97aa5ef9be81cb9ed20d9e5261575ab9d031
Hosein <mrqasemizade@gmail.com>
2022-04-07 16:28:25 +0430

    Draw square

---
+++ Draw_square/Draw_square/Draw_square/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int side = Convert.ToInt32(Console.ReadLine());
+                int counter = 0;
+                for (int height = 0; height < side/2; height++)
+                {
+                    for (int width = 0; width < side; width++)
+                    {
+                        if (height == counter)
+                            Console.Write("*");
+                        else
+                            Console.Write("*");
+                    }
+                    counter++;
+
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            
+        }
+    }
+}
