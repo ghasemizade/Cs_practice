d1d88fe4db6b4d8c6d8f2135bb6e31ff70d23f34
Hosein <mrqasemizade@gmail.com>
2022-04-07 14:39:17 +0430

    X pow Y

---
+++ X pow Y/X pow Y/X pow Y/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace XPOWERY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter two numbers till to X power of Y :");
+            int numX = Convert.ToInt32(Console.ReadLine());
+            int numY = Convert.ToInt32(Console.ReadLine());
+            double powXY = Math.Pow(numX,numY);
+
+            Console.WriteLine(powXY);
+        }
+    }
+}
