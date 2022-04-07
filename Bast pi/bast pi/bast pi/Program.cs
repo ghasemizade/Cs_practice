deb2ffaca9805f2c25515fa2a8f4b4a9ad947c5a
Hosein <mrqasemizade@gmail.com>
2022-04-07 18:35:59 +0430

    Bast pi

---
+++ Bast pi/bast pi/bast pi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Bastpi
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("enter the number to calculate bastpi :");
+            int number = Convert.ToInt32(Console.ReadLine());
+            float pi = 3.14F;
+
+            double circumference = 2 * pi * number;
+            pi = Convert.ToInt64(circumference / number);
+
+            Console.WriteLine(pi);
+
+        }
+    }
+}
