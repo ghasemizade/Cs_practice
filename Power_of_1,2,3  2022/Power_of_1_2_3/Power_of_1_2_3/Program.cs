caa7967036358c0152afae54a351bbf7cdf70221
Hosein <mrqasemizade@gmail.com>
2022-04-07 14:33:10 +0430

    power of 1,2,3

---
+++ Power_of_1,2,3  2022/Power_of_1_2_3/Power_of_1_2_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace POWER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number till to power of the 1,2,3 :");
+            int num = Convert.ToInt32(Console.ReadLine());
+            double pow1 = Math.Pow(num, 1);
+            double pow2 = Math.Pow(num, 2);
+            double pow3 = Math.Pow(num, 3);
+
+            Console.WriteLine(pow1);
+            Console.WriteLine(pow2);
+            Console.WriteLine(pow3);
+        }
+    }
+}
