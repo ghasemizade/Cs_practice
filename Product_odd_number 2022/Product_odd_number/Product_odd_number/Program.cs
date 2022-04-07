804fe8e3f55b341c03992bf68b33cd5ff0180333
Hosein <mrqasemizade@gmail.com>
2022-04-07 14:50:44 +0430

    Product Odd number

---
+++ Product_odd_number 2022/Product_odd_number/Product_odd_number/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter two number for calculate product of between odd number :");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int sum2  = 0;
+            int sum1 = 0;
+
+            if (num1 > num2)
+            {
+                for (int counter = num2; counter < num1; counter++)
+                {
+                    if (counter % 2 == 1)
+                    {
+                        sum1 *= counter;
+                    }
+                }
+                Console.WriteLine(sum1);
+            }
+            if (num1 < num2)
+            {
+                for (int counter = num1; counter < num2; counter++)
+                {
+                    if (counter % 2 == 1)
+                    {
+                        sum2 *= counter;
+                    }
+                }
+                Console.WriteLine(sum2);
+            }
+        }
+    }
+}
