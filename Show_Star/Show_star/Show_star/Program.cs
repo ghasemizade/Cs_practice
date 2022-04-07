6f2b8123846ab6542c4f1305dee804a092022418
Hosein <mrqasemizade@gmail.com>
2022-04-07 15:27:24 +0430

    Show star

---
+++ Show_Star/Show_star/Show_star/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number of star to show :");
+            int star = Convert.ToInt32(Console.ReadLine());
+
+            for (int counter = 1; counter <= star; counter++)
+            {
+                Console.WriteLine("*");
+            }
+        }
+    }
+}
