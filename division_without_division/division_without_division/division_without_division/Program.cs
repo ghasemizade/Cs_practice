667b3952aebfb29e42cc9444061e7dcefb3364f5
Hosein <mrqasemizade@gmail.com>
2022-04-07 15:15:49 +0430

    division_without_division

---
+++ division_without_division/division_without_division/division_without_division/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int div1 = 0;
+            int mod = num1;
+            for (int counter = 0;; counter++)
+                if (mod >= num2)
+                {
+                    div1++;
+                    mod -= num2;
+                }
+                else
+                {
+                    break;
+                }
+            Console.WriteLine(div1);
+        }
+    }
+}
