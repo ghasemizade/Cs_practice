bcd2fbd9ca32ce466716fc9d2d1f8e497cf01aec
Hosein <mrqasemizade@gmail.com>
2022-04-07 15:22:42 +0430

    Divine Propotion

---
+++ Divine Proportion/Divine Proportion/Divine Proportion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Divine_Proportion
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("enter number to Divine Proportion :");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(number / 1.618);
+            Console.WriteLine(number * 1.618);
+        }
+    }
+}
