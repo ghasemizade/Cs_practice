d7366e62f8017e8a6bdcc6ff7f66990f39d2d567
Hosein <mrqasemizade@gmail.com>
2022-04-07 14:07:42 +0430

    calculate of environment & area & diameter

---
+++ radius_of_circle 2022/radius_of_circle/radius_of_circle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWrold
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            float pi = 3.14F;
+            Console.WriteLine("Enter the radius of circle :");
+            float radius = Convert.ToSingle(Console.ReadLine());
+            float environment = (2 * pi * radius);
+            float diameter = (2 * radius);
+            float area = (pi * radius * radius);
+            Console.WriteLine("environment :");
+            Console.WriteLine(environment);
+            Console.WriteLine("diameter :");
+            Console.WriteLine(diameter);
+            Console.WriteLine("area :");
+            Console.WriteLine(area);
+        }
+    }
+}
