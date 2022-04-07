a72dbe729836fc67a362fb9138b6b13cb4e33dbc
Hosein <mrqasemizade@gmail.com>
2022-04-07 14:22:25 +0430

    Triangle Validation

---
+++ Triangle_validation 2022/Triangle Validation/Triangle Validation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace triangle
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the three side of triangle :");
+            int sideA = Convert.ToInt32(Console.ReadLine());
+            int sideB = Convert.ToInt32(Console.ReadLine());
+            int sideC = Convert.ToInt32(Console.ReadLine());
+
+            if (sideA + sideB > sideC && sideA + sideC > sideB && sideC + sideB > sideA)
+            {
+                Console.WriteLine("this numbers create triangle :)");
+            }
+            else
+            {
+                Console.WriteLine("this numbers does not create triangle !");
+            }
+        }
+    }
+}
