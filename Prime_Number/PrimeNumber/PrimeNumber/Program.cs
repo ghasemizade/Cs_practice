2e9787ad778e6316f7c06cb43f934bfe5396abb9
Hosein <mrqasemizade@gmail.com>
2022-04-07 17:30:53 +0430

    Prime_Number

---
+++ Prime_Number/PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace primenumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number to check PrimeNumber :");
+            int inputuser = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int temp = inputuser / 2;
+            int newtemp = 0;
+            for (int counter = 2; counter <= temp; counter++)
+            {
+                if (inputuser % counter == 0)
+                {
+                    Console.WriteLine("No");
+                    newtemp = 1;
+                    break;
+                }
+            }
+            if (newtemp == 0)
+            {
+                Console.WriteLine("Yes");
+            }
+        }
+    }
+}
