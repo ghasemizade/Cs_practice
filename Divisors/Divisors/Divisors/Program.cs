334f555264c11eaa27124d297fa8f040059dc040
Hosein <mrqasemizade@gmail.com>
2022-04-08 15:35:36 +0430

    Divisors

---
+++ Divisors/Divisors/Divisors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace divisor
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("enter the number :");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int counter = 1; counter <= number; counter++)
+            {
+                if (number % counter == 0)
+                {
+                    Console.WriteLine(counter);
+                }
+            }
+        }
+    }
+}
