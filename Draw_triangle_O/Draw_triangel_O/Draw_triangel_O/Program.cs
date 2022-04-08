f8d30b275fb2dc6157a1871ef463c02d3105448e
Hosein <mrqasemizade@gmail.com>
2022-04-08 16:12:13 +0430

    Draw_triangle_O

---
+++ Draw_triangle_O/Draw_triangel_O/Draw_triangel_O/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Triangle_O
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number side of star :");
+
+            int numstar = Convert.ToInt32(Console.ReadLine());
+
+            for (int counter = numstar; counter >=1; --counter)
+            {
+                //Console.WriteLine(" ** ");
+                for (int counter1 = 1; counter1 < counter; ++counter1)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
