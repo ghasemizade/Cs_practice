7fd95d8bf96f2c1eaa3e2f46edabbf3675536131
Hosein <mrqasemizade@gmail.com>
2022-04-08 15:49:05 +0430

    Draw_triangle_A

---
+++ Draw_triangle_A/draw_triangle_A/draw_triangle_A/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Triangle_A
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("enter the number side of star :");
+
+            int numstar = Convert.ToInt32 (Console.ReadLine());
+
+            for (int counter = 1; counter < numstar; counter++)
+            {
+                Console.WriteLine(" ** ");
+                for (int counter1 = 0; counter1 < counter; counter1++)
+                {
+                    Console.Write(" *** ");
+                }
+            }
+        }
+    }
+}
