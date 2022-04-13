a52e4511b857d8ad7e11691240aa40a729feaf0b
Hosein <mrqasemizade@gmail.com>
2022-04-13 12:38:58 +0430

    Sorted_Array

---
+++ Sorted_Array/Sorted_Array/Sorted_Array/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace sorted
+{
+    class Program
+    {
+        static void Main ()
+        {
+            Console.WriteLine ("Do you want Ascending sort press 'a' , 'A' or Desending sort press 'd' , 'D' :");
+            char sorting = Convert.ToChar (Console.ReadLine ());
+
+            Console.WriteLine("Enter the number of array :");
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            int[] myarray = new int[input];
+            Console.WriteLine("enter element of array :");
+            for (int i = 0; i < input; i++)
+            {
+                myarray[i] = Convert.ToInt32(Console.ReadLine()); 
+            }
+
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("Original array");
+            foreach (int i in myarray)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            if (sorting == 'a' || sorting == 'A')
+            {
+               Array.Sort (myarray);
+                Console.WriteLine("Ascending sort");
+                foreach (int i in myarray)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+
+            if (sorting == 'd' || sorting == 'D')
+            {
+                Array.Reverse(myarray);
+                Console.WriteLine("Desending sort");
+                foreach (int i in myarray)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
